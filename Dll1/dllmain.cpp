aa7f6787a03a21cdf8b1a1b813c90654aa86e303
Esinaliev <granulboy@gmail.com>
2023-03-20 23:25:14 +0600

    Добавьте файлы проекта.

---
+++ Dll1/dllmain.cpp
@@ -0,0 +1,27 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <cmath>
+
+#define EXPORTING_METHOD extern "C" __declspec(dllexport)
+
+EXPORTING_METHOD
+double MySqrt(int x) {
+    return std::sqrt(x);
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ HW02.12.22/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HW02._12._22
+{
+    class Program
+    {
+        private const string dllPath = @"C:\Users\GranullBoy\source\repos\HW02.12.22\x64\Debug\Dll1.dll";
+
+        [DllImport(dllPath)]
+        private static extern double MySqrt(int x);
+        static void Main(string[] args)
+        {
+            int x;
+            x = int.Parse(Console.ReadLine());
+            double result = MySqrt(x);
+            Console.WriteLine(result);
+        }
+    }
+}
